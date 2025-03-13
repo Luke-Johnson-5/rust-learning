b8cde2d142e00efa9d8e74cf9ebfb617935a7c39
Luke Johnson <luke.johnson@gwmail.gwu.edu>
2025-03-13 15:42:16 -0400

    chapter 3

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\theoz\projects_rust\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\theoz\projects_rust\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\theoz\projects_rust\variables\target\debug\variables.exe: C:\Users\theoz\projects_rust\variables\src\main.rs
