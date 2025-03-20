433d4ca8b8130d79453313e05c692a2d61e09a6b
Luke Johnson <luke.johnson@gwmail.gwu.edu>
2025-03-20 14:43:19 -0400

    loops and ifs

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -13,4 +13,15 @@ fn main() {
     let number = if condition { 5 } else { 6 };
 
     println!("The value of number is: {number}");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
