1fcac97143749e4c3dd106b8a77c636c6fed9e71
Luke Johnson <luke.johnson@gwmail.gwu.edu>
2025-03-20 19:17:27 -0400

    nth fib

---
+++ nthfib/target/debug/deps/nthfib.d
@@ -0,0 +1,5 @@
+C:\Users\theoz\projects_rust\nthfib\target\debug\deps\nthfib.exe: src\main.rs
+
+C:\Users\theoz\projects_rust\nthfib\target\debug\deps\nthfib.d: src\main.rs
+
+src\main.rs:
---
+++ nthfib/target/debug/nthfib.d
@@ -0,0 +1,1 @@
+C:\Users\theoz\projects_rust\nthfib\target\debug\nthfib.exe: C:\Users\theoz\projects_rust\nthfib\src\main.rs
