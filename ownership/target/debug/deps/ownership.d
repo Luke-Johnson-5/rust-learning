0cb8a6bfe02a783f1d9d3785c7247300a0d1d8f8
Luke Johnson <luke.johnson@gwmail.gwu.edu>
2025-03-21 21:11:13 -0400

    ownership 5.1

---
+++ ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+C:\Users\theoz\projects_rust\ownership\target\debug\deps\ownership.exe: src\main.rs
+
+C:\Users\theoz\projects_rust\ownership\target\debug\deps\ownership.d: src\main.rs
+
+src\main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\Users\theoz\projects_rust\ownership\target\debug\ownership.exe: C:\Users\theoz\projects_rust\ownership\src\main.rs
